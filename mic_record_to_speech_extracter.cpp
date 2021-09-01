022249c8d23910b96ca2ff46a4cb0ad70467dc5d
D1ngn <naganod1@gmail.com>
2021-09-01 16:29:04 +0900

    display recog_result and localization_result

--- mic_record_to_speech_extracter.cpp
+++ mic_record_to_speech_extracter.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
@@ -17,7 +18,11 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+// 関数プロトタイプ宣言
+vector<string> split(string str, char delimiter);
+
+int main(int argc, char *argv[]) 
+{
 	// 録音（キャプチャ）用の設定
 	int i;
 	int buffer_size;
@@ -26,7 +31,7 @@ int main(int argc, char *argv[]) {
 	int buffer_frames = 128;
 	unsigned int sample_rate = 16000;
 	unsigned int channel = 8;
-	int audio_length = 3; // second
+	unsigned int audio_length = 3; // second
 	snd_pcm_t *capture_handle;
 	snd_pcm_hw_params_t *hw_params;
 	snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE; // Signed 16 bit Little Endian
@@ -36,9 +41,15 @@ int main(int argc, char *argv[]) {
 	int sockfd;
     struct sockaddr_in addr;
     char receive_data[30]; // 受信データ
+	char delimiter = '|'; // 受け取った音声認識結果と音源定位結果を区切る文字
+	const char *recog_result_temp;
+	char *recog_result;
+	const char *localization_result;
+	 
 	// ソケット生成
 	// AF_INETはIPv4、SOCK_STREAMはTCPであることを表す
-    if((sockfd = socket( AF_INET, SOCK_STREAM, 0)) < 0){
+    if((sockfd = socket( AF_INET, SOCK_STREAM, 0)) < 0)
+	{
         perror("socket");
     }
 	// 送信先アドレス・ポート番号設定
@@ -57,17 +68,20 @@ int main(int argc, char *argv[]) {
 	 * オープンモード
 	 *
 	 **/
-	if((err = snd_pcm_open(&capture_handle, argv[1], SND_PCM_STREAM_CAPTURE, 0)) < 0) {
+	if((err = snd_pcm_open(&capture_handle, argv[1], SND_PCM_STREAM_CAPTURE, 0)) < 0) 
+	{
 		fprintf(stderr, "cannot open audio device %s (%s)\n", argv[1], snd_strerror(err));
 		exit(1);
 	}
 	// mallocを使ってハードウェアパラメーターのコンフィグレーション領域を確保する
-	if((err = snd_pcm_hw_params_malloc(&hw_params)) < 0) {
+	if((err = snd_pcm_hw_params_malloc(&hw_params)) < 0) 
+	{
 		fprintf(stderr, "cannot allocate hardware parameter structure (%s)\n", snd_strerror(err));
 		exit(1);
 	}
 	//コンフィグレーション領域を初期化する
-	if((err = snd_pcm_hw_params_any(capture_handle, hw_params)) < 0) {
+	if((err = snd_pcm_hw_params_any(capture_handle, hw_params)) < 0) 
+	{
 		fprintf(stderr, "cannot initialize hardware parameter structure (%s)\n", snd_strerror(err));
 		exit(1);
 	}
@@ -80,7 +94,8 @@ int main(int argc, char *argv[]) {
 	 *
 	 * SND_PCM_ACCESS_RW_INTERLEAVED = snd_pcm_readi/snd_pcm_writei access
 	 */
-	if((err = snd_pcm_hw_params_set_access(capture_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
+	if((err = snd_pcm_hw_params_set_access(capture_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) 
+	{
 		fprintf(stderr, "cannot set access type (%s)\n", snd_strerror(err));
 		exit(1);
 	}
@@ -91,7 +106,8 @@ int main(int argc, char *argv[]) {
 	 * コンフィグレーション領域 (snd_pcm_hw_params *)
 	 * サンプリングフォーマット (snd_pcm_format_t)
 	 */
-	if((err = snd_pcm_hw_params_set_format(capture_handle, hw_params, format)) < 0) {
+	if((err = snd_pcm_hw_params_set_format(capture_handle, hw_params, format)) < 0) 
+	{
 		fprintf(stderr, "cannot set sample format (%s)\n", snd_strerror(err));
 		exit(1);
 	}
@@ -103,7 +119,8 @@ int main(int argc, char *argv[]) {
 	 * 大凡のサンプリングレート (unsigned int *)
 	 * サブユニットの方向
 	 */
-	if((err = snd_pcm_hw_params_set_rate_near(capture_handle, hw_params, &sample_rate, 0)) < 0) {
+	if((err = snd_pcm_hw_params_set_rate_near(capture_handle, hw_params, &sample_rate, 0)) < 0) 
+	{
 		fprintf(stderr, "cannot set sample rate (%s)\n", snd_strerror(err));
 		exit(1);
 	}
@@ -114,19 +131,22 @@ int main(int argc, char *argv[]) {
 	 * コンフィグレーション領域 (snd_pcm_hw_params *)
 	 * チャネル数 (unsigned int)
 	 */
-	if((err = snd_pcm_hw_params_set_channels(capture_handle, hw_params, channel)) < 0) {
+	if((err = snd_pcm_hw_params_set_channels(capture_handle, hw_params, channel)) < 0) 
+	{
 		fprintf(stderr, "cannot set channel count (%s)\n", snd_strerror(err));
 		exit(1);
 	}
 	//コンフィグレーションを設定
-	if((err = snd_pcm_hw_params(capture_handle, hw_params)) < 0) {
+	if((err = snd_pcm_hw_params(capture_handle, hw_params)) < 0) 
+	{
 		fprintf(stderr, "cannot set hw_params (%s)\n", snd_strerror(err));
 		exit(1);
 	}
 	//コンフィグレーション領域の開放
 	snd_pcm_hw_params_free(hw_params);
 	// オーディオインターフェースの開始
-	if((err = snd_pcm_prepare(capture_handle)) < 0) {
+	if((err = snd_pcm_prepare(capture_handle)) < 0) 
+	{
 		fprintf(stderr, "cannot prepare audio interface for use (%s)", snd_strerror(err));
 		exit(1);
 	}
@@ -134,7 +154,8 @@ int main(int argc, char *argv[]) {
 	// キャプチャー用のバッファーのサイズを算出
 	buffer_size = buffer_frames * snd_pcm_format_width(format)/8 * channel; /* 2 bytes/sample, 8 channels */
 	// 録音データを一定時間ごとに区切り、speech extracterモジュール（提案システム）に送り続ける
-	while(true){
+	while(true)
+	{
 		// 音声保存用ファイルの名前につける日付の設定
 		time_t now = time(nullptr); // 現在時刻（1970年1月1日からの経過秒）
 		//形式を変換する    
@@ -157,8 +178,10 @@ int main(int argc, char *argv[]) {
 		// キャプチャー用のバッファーを確保
 		buffer = (char*) malloc(buffer_size);
 		// キャプチャー
-		for(i = 0; i < sample_rate*audio_length/buffer_frames; i++) {
-			if((err = snd_pcm_readi(capture_handle, buffer, buffer_frames)) != buffer_frames) {
+		for(i = 0; i < sample_rate*audio_length/buffer_frames; i++) 
+		{
+			if((err = snd_pcm_readi(capture_handle, buffer, buffer_frames)) != buffer_frames) 
+			{
 				fprintf(stderr, "read from audio interface failed (%s)\n", snd_strerror(err));
 				exit(1);
 			}
@@ -170,10 +193,27 @@ int main(int argc, char *argv[]) {
 		// データ受信
 		// サーバーからの返答を受信
 		recv(sockfd, receive_data, 30, 0);
-		// 受信データを標準出力
-		cout << receive_data << endl;
+		// 受信データを音声認識結果と音源定位結果に分けて標準出力
+		int display_counter = 0;
+		// Pythonのリストの要素をfor文で取り出すのに似た処理
+		for (const auto subStr : split(receive_data, delimiter)) 
+		{
+			if(display_counter==0)
+			{
+				recog_result_temp = subStr.c_str(); // const char型に変換
+				// ポインタの参照先が音源定位結果で書き換えられてしまうためstrcpyでコピー
+				strcpy(recog_result, recog_result_temp);
+				cout << "音声認識結果：" << recog_result << endl;
+			} else if(display_counter==1) 
+			{
+				localization_result = subStr.c_str();
+				cout << "音源定位結果：" << localization_result << endl;
+			}
+			display_counter++;
+		}
 		// メモリの開放
-		if (buffer != NULL) {
+		if (buffer != NULL) 
+		{
 			free(buffer);
 		}
 		// ファイルを閉じる
@@ -182,9 +222,34 @@ int main(int argc, char *argv[]) {
 	// ソケットを閉じる
 	close(sockfd);
 	// ストリームを閉じる
-	if (capture_handle != NULL) {
+	if (capture_handle != NULL) 
+	{
 		snd_pcm_close(capture_handle);
 	}
 	exit(0);
   	return 0;
+}
+
+// stringを特定の文字でsplitする関数
+vector<string> split(string str, char delimiter) 
+{
+    int first = 0;
+    int last = str.find_first_of(delimiter);
+	// vectorは、長さを変えることのできる配列
+    vector<std::string> result;
+	// 区切り文字が見つかったところまで文字列を取得してvectorに追加
+    while (first < str.size()) {
+        string subStr(str, first, last - first);
+ 
+        result.push_back(subStr);
+ 
+        first = last + 1;
+		// 区切り文字を検索
+        last = str.find_first_of(delimiter, first);
+ 
+        if (last == string::npos) {
+            last = str.size();
+        }
+    }
+    return result;
 }
